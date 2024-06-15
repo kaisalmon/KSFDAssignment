94e639a0b6204013e99131b50631b643593838c7
Kai Salmon <kaisalmon@hotmail.com>
2024-06-15 21:18:37 +0100

    Added tests

---
+++ src/tests/test_csvloader.py
@@ -0,0 +1,55 @@
+import unittest
+from unittest.mock import patch, Mock
+from secondary_adapters.csvloader import load_dataset
+from logic.movie import Movie
+from main_types import Config
+
+class TestLoadDataset(unittest.TestCase):
+    def setUp(self):
+        self.config = Config(
+            logger=Mock(),
+            dataset_path="",
+            workspace_path="",
+            query_path="",
+        )  
+    
+    # See https://stackoverflow.com/questions/1289894/how-do-i-mock-an-open-used-in-a-with-statement-using-the-mock-framework-in-pyth 
+    # for info on mocking the open built in
+    @patch('builtins.open')
+    def test_load_dataset_success(self, mock_open):
+        # Arrange
+        csv_data = '''record_id,movie_id,title,country,release_year,description,vector_embedding
+1,1,"Movie 1",US,2020,"Description 1","[0.1, 0.2, 0.3]"
+2,2,"Movie 2",UK,2021,"Description 2","[0.4, 0.5, 0.6]"'''
+        mock_open.return_value.__enter__.return_value = csv_data.splitlines()
+
+        # Act
+        result = load_dataset('dataset.csv', self.config)
+
+        # Assert
+        self.assertEqual(len(result), 2)
+        self.assertIsInstance(result[0], Movie)
+        self.assertIsInstance(result[1], Movie)
+        self.config.logger.debug.assert_called_once_with("loaded 2 records")
+        self.config.logger.warning.assert_not_called()
+
+    @patch('builtins.open')
+    def test_load_dataset_error(self, mock_open):
+        # Arrange
+        csv_data = '''record_id,movie_id,title,country,release_year,description,vector_embedding
+1,1,"Movie 1",US,2020,"Description 1","[0.1, 0.2, 0.3]"
+2,2,"Movie 2",UK,invalid_year,"Description 2","[0.4, 0.5, 0.6]"'''
+        mock_open.return_value.__enter__.return_value = csv_data.splitlines()
+
+        # Act
+        result = load_dataset('dataset.csv', self.config)
+
+        # Assert
+        self.assertEqual(len(result), 1)
+        self.assertIsInstance(result[0], Movie)
+        self.config.logger.debug.assert_called_once_with("loaded 1 records")
+        self.config.logger.warning.call_count == 2 
+        self.config.logger.warning.assert_called_with("Encountered 1 error(s)")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/tests/test_vectordb.py
@@ -0,0 +1,40 @@
+import unittest
+from logic.movie import Movie, MovieQuery
+from secondary_adapters.vectordb import search_movies
+
+class TestSearch2DMovies(unittest.TestCase):
+    def setUp(self):
+        self.movies = [
+            Movie(record_id=1, movie_id=1, title="North", country="A", release_year=1, description="Blah", vector_embedding=[0, 1]),
+            Movie(record_id=2, movie_id=2, title="East", country="B", release_year=2, description="Blah", vector_embedding=[1, 0]),
+            Movie(record_id=3, movie_id=3, title="South", country="B", release_year=2, description="Blah", vector_embedding=[0, -1]),
+            Movie(record_id=4, movie_id=4, title="West", country="C", release_year=4, description="Blah", vector_embedding=[-1, 0]),
+        ]
+
+    def test_search_movies(self):
+        query = MovieQuery(min_release_year=0, max_release_year=5, query="North West West", vector_embedding=[-0.6, 0.3])
+        results = search_movies(self.movies, query, limit=2)
+        north_result = next(((movie, distance)  for (movie, distance) in results if movie.title == "North"), None)
+        west_result = next(((movie, distance)  for (movie, distance) in results if movie.title == "West"), None)
+        assert west_result, "One of the results should be West"
+        assert north_result, "One of the results should be North"
+
+    def test_search_movies_filter_high(self):
+        query = MovieQuery(min_release_year=4, max_release_year=5, query="North West West", vector_embedding=[-0.6, 0.3])
+        results = search_movies(self.movies, query, limit=4)
+        assert len(results)==1, "Only one result should be returned"
+        (movie, _) = results[0]
+        assert movie.title == "West"
+        
+    def test_search_movies_filter_low(self):
+        query = MovieQuery(min_release_year=1, max_release_year=1, query="North West West", vector_embedding=[-0.6, 0.3])
+        results = search_movies(self.movies, query, limit=4)
+        assert len(results)==1, "Only one result should be returned"
+        (movie, _) = results[0]
+        assert movie.title == "North"
+
+   
+   
+
+if __name__ == '__main__':
+    unittest.main()
